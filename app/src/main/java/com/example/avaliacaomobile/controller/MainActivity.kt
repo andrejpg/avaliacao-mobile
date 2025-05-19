e73c94a91351d4ba6fcd9a2bce68964863cb8df9
andrejpg <andremoraisbrito556@gmail.com>
2025-05-19 18:16:18 -0300

    refactor: arruma divisão de pacotes no projeto

--- app/src/main/java/com/example/avaliacaomobile/controller/MainActivity.kt
+++ app/src/main/java/com/example/avaliacaomobile/controller/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.avaliacaomobile
+package com.example.avaliacaomobile.controller
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.avaliacaomobile.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
