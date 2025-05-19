3736069d645d323d51415571380e4d11d70476fc
Viviane Mori <vivianeendomori@gmail.com>
2025-05-19 19:14:05 -0300

    feat: add elementos no front da main activity

--- app/src/main/java/com/example/avaliacaomobile/controller/MainActivity.kt
+++ app/src/main/java/com/example/avaliacaomobile/controller/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.avaliacaomobile.controller
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.avaliacaomobile.R
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    lateinit var carsListView: ListView
+    lateinit var listAdapter: ArrayAdapter<String>
+    lateinit var carsList: ArrayList<String>
+    lateinit var searchView: SearchView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +27,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        carsListView = findViewById(R.id.listView)
+        searchView = findViewById(R.id.searchView)
+
+        carsList = ArrayList()
+        carsList.add("Honda HR-V")
+        carsList.add("Renault Kwid")
+        carsList.add("Volskwagen Polo")
+        listAdapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            carsList
+        )
+        carsListView.adapter = listAdapter
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (carsList.contains(query)) {
+                    listAdapter.filter.filter(query)
+                } else {
+                    Toast.makeText(this@MainActivity, "Sem resultados.", Toast.LENGTH_LONG).show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                listAdapter.filter.filter(newText)
+                return false
+            }
+        })
     }
 }
